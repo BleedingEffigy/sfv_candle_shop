3df8e4d838b90eb59be20bc2246898636beb5539
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-16 11:03:33 -0700

    added color change action to reducers

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,18 @@
-const initialState = {
+import {CHANGE_CANDLE_COLOR} from "../actions/index.js"
 
+const initialState = {
+  candle_colors: {}
 }
 
 function rootReducer(state = initialState, action) {
+  switch(action.type){
+    case CHANGE_CANDLE_COLOR:
+      new_colors = Object.assign({}, state.candle_colors, action.payload)
+      return Object.assign({}, state, {
+        candle_colors: new_colors
+      })
+      break
+  }
   return state
 }
 
