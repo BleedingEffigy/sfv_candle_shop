61b74dc4c39a4cedfee85fffe04a68b007d3ad84
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-17 10:13:05 -0700

    cleaned up index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 import "./sitestyles.scss"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import Navigation from "../components/navigation"
 import Landing from "../components/landing-banner"
-import CandlePage from "../pages/candles"
 
 const IndexPage = () => (
   <div>
