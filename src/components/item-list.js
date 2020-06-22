21aa1660fb9d0cf068ff0c45dcc428953a2eb51d
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-22 08:47:52 -0700

    Remade item-list into a functional component

--- src/components/item-list.js
+++ src/components/item-list.js
@@ -1,29 +1,23 @@
 import React from "react"
 import ItemCard from "./item-card"
 
-class ItemList extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      items: {
-        "one": {"shape":"square","color":"red"},
-        "two": {"shape":"etched stripes","color":"violet"},
-        "three": {"shape":"pyramid","color":"yellow"},
-        "four": {"shape":"round","color":"green"},
-        "five": {"shape":"square","color":"blue"},
+const state = {
+        items: {
+          "one": {"shape":"square","color":"red"},
+          "two": {"shape":"etched stripes","color":"violet"},
+          "three": {"shape":"pyramid","color":"yellow"},
+          "four": {"shape":"round","color":"green"},
+          "five": {"shape":"square","color":"blue"},
+        }
       }
-    }
-  }
 
-  render(){
-    return (
-      <div className="columns is-multiline">
-        {Object.entries(this.state.items).map(([key, item]) =>
-          <div className="column is-4" key={key}><ItemCard shape={item.shape} color={item.color}/></div>
-        )}
-      </div>)
-  }
-}
+const ItemList  = () => (
+  <div className="columns is-multiline">
+    {Object.entries(state.items).map(([key, item]) =>
+      <div className="column is-4" key={key}><ItemCard shape={item.shape} color={item.color}/></div>
+    )}
+  </div>
+)
 
 
 export default ItemList
