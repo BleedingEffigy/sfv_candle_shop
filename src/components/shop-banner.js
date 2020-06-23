6ef9264d88cd4c52e31e83c11d2217e9590b6fbf
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-23 12:26:47 -0700

    changed instances of "class" to "className"

--- src/components/shop-banner.js
+++ src/components/shop-banner.js
@@ -2,11 +2,11 @@ import React from "react"
 
 const ShopBanner = () => (
   <div>
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <div class="container">
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
           <h1 className="title is-1 has-text-dark">The title 2 heading</h1>
-          <h2 class="subtitle">
+          <h2 className="subtitle">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
           </h2>
         </div>
