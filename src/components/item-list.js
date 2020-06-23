0d449ca33419bbb2f19e0ee38065888452309631
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-23 12:02:05 -0700

    Removed state from component as it sources from store now

--- src/components/item-list.js
+++ src/components/item-list.js
@@ -3,16 +3,6 @@ import ItemCard from "./item-card"
 
 import { connect } from "react-redux"
 
-const state = {
-        items: {
-          "one": {"shape":"square","color":"red"},
-          "two": {"shape":"etched stripes","color":"violet"},
-          "three": {"shape":"pyramid","color":"yellow"},
-          "four": {"shape":"round","color":"green"},
-          "five": {"shape":"square","color":"blue"},
-        }
-      }
-
 const select = state => {
   return { candle_colors: state.candle_colors}
 }
