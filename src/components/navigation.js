9259fcfe54b3ad4a34d27791d2119b22ec845939
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-23 12:25:53 -0700

    changed instances of "class" to "className"

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,37 +2,37 @@ import React from "react"
 import { Link } from "gatsby"
 
 const Navigation = () => (
-  <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="/">
+  <nav className="navbar" role="navigation" aria-label="main navigation">
+  <div className="navbar-brand">
+    <a className="navbar-item" href="/">
       <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28"></img>
     </a>
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
+  <div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start">
+      <a className="navbar-item">
         <Link to="/candles">Shop</Link>
       </a>
 
-      <a class="navbar-item">
+      <a className="navbar-item">
         Contact
       </a>
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+          <a className="button is-primary">
             <Link to="/cart">Cart</Link>
           </a>
-          <a class="button is-light">
+          <a className="button is-light">
             Log in
           </a>
         </div>
