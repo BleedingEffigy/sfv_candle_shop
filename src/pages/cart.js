5dd57c86bfae9eea4bf3e6511a166fe5353ecdbb
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-25 11:32:23 -0700

    Connected cart to dispatch, replaced columns with table

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -3,9 +3,16 @@ import Navigation from "../components/navigation"
 import CartTotal from "../components/cart-total"
 
 import { connect } from "react-redux"
+import {addToCart} from "../js/actions/index"
 
 const select = state => {
   return { cart: state.cart}
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addToCart: cart => dispatch(addToCart(cart))
+  }
 }
 
 const ConnectedCartPage = ({cart}) => (
@@ -13,56 +20,45 @@ const ConnectedCartPage = ({cart}) => (
     <Navigation/>
     <div className="columns is-centered">
       <div className="column is-10">
-
         <div className="columns">
           <div className="column is-8">
-            <div className="columns">
-              <div className="column is-offset-2 has-text-weight-bold">
-                <p>PRODUCT</p>
-              </div>
-              <div className="column is-offset-5 has-text-weight-bold">
-                <p>PRICE</p>
-              </div>
-              <div className="column has-text-weight-bold">
-                <p>QUANTITY</p>
-              </div>
-              <div className="column has-text-weight-bold">
-                <p>SUBTOTAL</p>
-              </div>
-            </div>
-
-            {Object.entries(cart).map(([key, val]) =>
-              <div className="columns is-vcentered">
-                <div className="column">
-                  <figure className="image is-128x128">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png"></img>
-                  </figure>
-                </div>
-                <div className="column">
-                  <p>PRODUCT</p>
-                </div>
-                <div className="column is-offset-5">
-                  <p>PRICE</p>
-                </div>
-                <div className="column">
-                  <p>QUANTITY</p>
-                </div>
-                <div className="column">
-                  <p>SUBTOTAL</p>
-                </div>
-              </div>
-            )}
+            <table className="table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>PRODUCT</th>
+                  <th>PRICE</th>
+                  <th>QUANTITY</th>
+                  <th>SUBTOTAL</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.entries(cart).map(([key, val]) =>
+                  <tr>
+                    <td>
+                      <figure className="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png"></img>
+                      </figure>
+                    </td>
+                    <td style={{verticalAlign: "middle"}}>PRODUCT</td>
+                    <td style={{verticalAlign: "middle"}}>PRICE</td>
+                    <td style={{verticalAlign: "middle"}}>QUANTITY</td>
+                    <td style={{verticalAlign: "middle"}}>SUBTOTAL</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
           </div>
-
           <div className="column is-4">
             <CartTotal/>
           </div>
         </div>
+
       </div>
     </div>
   </div>
 )
 
-const CartPage = connect(select)(ConnectedCartPage)
+const CartPage = connect(select, mapDispatchToProps)(ConnectedCartPage)
 
 export default CartPage
