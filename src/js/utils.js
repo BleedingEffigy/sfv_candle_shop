652b33134c6de90a6bcd676c7026bd5c588fe3b9
Alexander Hernandez <bestrafermich@gmail.com>
2020-06-25 13:20:49 -0700

    Added function to create the SKU

--- src/js/utils.js
+++ src/js/utils.js
@@ -12,3 +12,36 @@ export const renderColorImage = (props) => {
       return "https://ii2.worldmarket.com/fcgi-bin/iipsrv.fcgi?FIF=/images/worldmarket/source/32470_XXX_v1.tif&wid=480&cvt=jpeg"
   }
 }
+
+export const createSKU = (shape, color) => {
+  const sku = "PC"
+  switch(shape){
+    case "square":
+      sku.concat("01")
+      break
+    case "etched-stripes":
+      sku.concat("02")
+      break
+    case "pyramid":
+      sku.concat("03")
+      break
+    case "round":
+      sku.concat("04")
+      break
+  }
+  switch(color){
+    case "red":
+      sku.concat("-R")
+      break
+    case "violet":
+      sku.concat("-V")
+      break
+    case "blue":
+      sku.concat("-B")
+      break
+    case "yellow":
+      sku.concat("-Y")
+      break
+  }
+  return sku
+}
