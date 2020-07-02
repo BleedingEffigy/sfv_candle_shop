549fb4b2b51cbf212a4e8aaf93e633655991ec5e
Alexander Hernandez <bestrafermich@gmail.com>
2020-07-02 02:34:30 -0700

    Updated cart-totals layout

--- src/components/cart-total.js
+++ src/components/cart-total.js
@@ -12,34 +12,39 @@ const ConnectedCartTotal = ({cart}) => (
     <h1 className="title">Cart Total</h1>
     <div className="level">
       <div className="level-left">
-        <p>subtotal</p>
+        <p className="has-text-weight-bold">subtotal</p>
       </div>
       <div className="level-right">
         <p>${sumQuantityPrice(cart)}</p>
       </div>
     </div>
+    <hr/>
     <br/>
     <div className="columns">
       <div className="column">
-        <p>shipping</p>
+        <p className="has-text-weight-bold">shipping</p>
       </div>
       <div className="column">
         <div className="control">
-          <label className="radio box">
+          <label className="radio">
             <input type="radio" name="shipping" defaultChecked/>Fast
-              <div className="has-text-grey-lighter">
+              <div className="has-text-grey-light">
                 (+$20)
               </div>
           </label>
           <label className="radio">
             <input type="radio" name="shipping"/>Standard
+              <div className="has-text-grey-light">
+                (+$6)
+              </div>
           </label>
         </div>
       </div>
     </div>
+    <hr/>
     <br/>
-    <p>Total</p>
-    <button className="button is-fullwidth" disabled title="We are currently not processing any orders">Proceed to checkout</button>
+    <p className="has-text-weight-bold">Total</p>
+    <button className="button is-fullwidth is-primary" disabled title="We are currently not processing any orders">Proceed to checkout</button>
   </div>
 )
 
