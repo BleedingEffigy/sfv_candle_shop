8d38520138cfed8c8e6a6bac66b0c4b3921ae08c
Alexander Hernandez <bestrafermich@gmail.com>
2020-07-02 11:28:01 -0700

    added text to shop-banner

--- src/components/shop-banner.js
+++ src/components/shop-banner.js
@@ -5,9 +5,9 @@ const ShopBanner = () => (
     <section className="hero is-primary">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title is-1 has-text-dark">The title 2 heading</h1>
+          <h1 className="title is-1 has-text-dark">Simply Made Candles</h1>
           <h2 className="subtitle">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
+            <p>Choose from a variety of shapes and sizes </p>
           </h2>
         </div>
       </div>
