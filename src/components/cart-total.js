79696dba2e73387a1a3c6d5e0a8edeee48100beb
Alexander Hernandez <bestrafermich@gmail.com>
2020-07-06 18:48:39 -0700

    Cart displays total, and fast shipping is disabled

--- src/components/cart-total.js
+++ src/components/cart-total.js
@@ -26,14 +26,14 @@ const ConnectedCartTotal = ({cart}) => (
       </div>
       <div className="column">
         <div className="control">
-          <label className="radio">
-            <input type="radio" name="shipping" defaultChecked/>Fast
+          <label className="radio" disabled>
+            <input type="radio" name="shipping" disabled/>Fast
               <div className="has-text-grey-light">
                 (+$20)
               </div>
           </label>
           <label className="radio">
-            <input type="radio" name="shipping"/>Standard
+            <input type="radio" name="shipping" defaultChecked/>Standard
               <div className="has-text-grey-light">
                 (+$6)
               </div>
@@ -43,7 +43,14 @@ const ConnectedCartTotal = ({cart}) => (
     </div>
     <hr/>
     <br/>
-    <p className="has-text-weight-bold">Total</p>
+    <div className="level">
+      <div className="level-left">
+        <p className="has-text-weight-bold">Total</p>
+      </div>
+      <div className="level-right">
+        <p>${sumQuantityPrice(cart)+6}</p>
+      </div>
+    </div>
     <button className="button is-fullwidth is-primary" disabled title="We are currently not processing any orders">Proceed to checkout</button>
   </div>
 )
