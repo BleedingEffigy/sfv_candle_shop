6194cf291d5bacb136e9748f24d318eb32b7a091
Alexander Hernandez <bestrafermich@gmail.com>
2020-07-08 16:47:02 -0700

    redefined image component to provide the images through graphql

--- src/components/image.js
+++ src/components/image.js
@@ -13,20 +13,75 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
+ export const candleImage = graphql`
+   fragment candleImage on File {
+     childImageSharp {
+       fixed(width: 400, height:300, cropFocus: CENTER, fit: FILL) {
+         ...GatsbyImageSharpFixed
+       }
+     }
+   }
+ `
+
+const Image = (props) => {
+
   const data = useStaticQuery(graphql`
     query {
       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
+        ...candleImage
+      }
+      blue_pyramid: file(relativePath: { eq: "blue_pyramid_candle.jpg" }) {
+        ...candleImage
+      }
+      blue_round: file(relativePath: { eq: "blue_round_candle.jpg" }) {
+        ...candleImage
+      }
+      blue_square: file(relativePath: { eq: "blue_square_candle.jpg" }) {
+        ...candleImage
+      }
+      red_pyramid: file(relativePath: { eq: "red_pyramid_candle.jpg" }) {
+        ...candleImage
+      }
+      red_round: file(relativePath: { eq: "red_round_candle.jpg" }) {
+        ...candleImage
+      }
+      red_square: file(relativePath: { eq: "red_square_candle.jpeg" }) {
+        ...candleImage
+      }
+      violet_pyramid: file(relativePath: { eq: "violet_pyramid_candle.jpg" }) {
+        ...candleImage
+      }
+      violet_round: file(relativePath: { eq: "violet_round_candle.jpg" }) {
+        ...candleImage
+      }
+      violet_square: file(relativePath: { eq: "violet_square_candle.jpg" }) {
+        ...candleImage
+      }
+      yellow_pyramid: file(relativePath: { eq: "yellow_pyramid_candle.jpg" }) {
+        ...candleImage
+      }
+      yellow_round: file(relativePath: { eq: "yellow_round_candle.jpg" }) {
+        ...candleImage
+      }
+      yellow_square: file(relativePath: { eq: "yellow_square_candle.jpg" }) {
+        ...candleImage
+      }
+      violet_etched_stripes: file(relativePath: { eq: "violet_round_candle.jpg" }) {
+        ...candleImage
+      }
+      red_etched_stripes: file(relativePath: { eq: "red_round_candle.jpg" }) {
+        ...candleImage
+      }
+      yellow_etched_stripes: file(relativePath: { eq: "yellow_round_candle.jpg" }) {
+        ...candleImage
+      }
+      blue_etched_stripes: file(relativePath: { eq: "blue_round_candle.jpg" }) {
+        ...candleImage
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fixed={data[`${props.color}_${props.shape}`].childImageSharp.fixed} />
 }
 
 export default Image
