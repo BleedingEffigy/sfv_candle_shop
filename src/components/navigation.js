c3459d86c18c55e0f5ce8ab2beb2e9724f22f2b1
Alexander Hernandez <bestrafermich@gmail.com>
2020-07-09 11:54:07 -0700

    navbar hamburger is clickable

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,38 +1,54 @@
 import React from "react"
 import { Link } from "gatsby"
 
-const Navigation = () => (
-  <nav className="navbar has-background-grey-darker" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand">
-    <Link className="navbar-item" to="/">
-      <p className="title logo has-text-white">SFV CANDLES</p>
-    </Link>
+class Navigation extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      active: false,
+    }
+    this.hamburgerClick = this.hamburgerClick.bind(this)
+  }
 
-    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+  hamburgerClick(){
+    this.setState({active: !this.state.active})
+  }
 
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-        <Link to="/candles" className="navbar-item has-text-white">Shop</Link>
+  render () {
+    return (
+      <nav className="navbar has-background-grey-darker" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <p className="title logo has-text-white">SFV CANDLES</p>
+          </Link>
 
-      <a className="navbar-item has-text-white">
-        Contact
-      </a>
-    </div>
-
-    <div className="navbar-end">
-      <div className="navbar-item">
-        <div className="buttons">
-            <Link to="/cart" className="button is-primary has-text-white">Cart</Link>
+          <a role="button" className="navbar-burger burger" onClick={this.hamburgerClick} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-      </div>
-    </div>
-  </div>
-</nav>
-)
+
+        <div id="navbarBasicExample" className={`navbar-menu has-background-grey-darker ${this.state.active ? "is-active": ""}`}>
+          <div className="navbar-start">
+              <Link to="/candles" className="navbar-item has-text-white">Shop</Link>
+
+            <a className="navbar-item has-text-white">
+              Contact
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                  <Link to="/cart" className="button is-primary has-text-white">Cart</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
 
 export default Navigation
