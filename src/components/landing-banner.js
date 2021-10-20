e60bfcd5a05b0516e13d7d7b7f5e0ae664bc78bd
Alex <ahern077@gmail.com>
2021-10-20 15:42:20 -0700

    added soft UI to landing page

--- src/components/landing-banner.js
+++ src/components/landing-banner.js
@@ -4,43 +4,28 @@ import SEO from "../components/seo"
 
 const Landing = () => (
   <div>
-    <section className="hero is-primary is-fullheight">
-      <div className="hero-body">
-        <div className="container">
-          <SEO title="Home" />
-          <div className="columns is-gapless">
-            <div className="column is-6">
-              <figure className="image is-3by2">
-                <img src="https://cdn18.picryl.com/photo/2019/10/07/san-fernando-valley-california-e90e48-1024.jpg"></img>
-              </figure>
-            </div>
-            <div className="column is-6 is-flex">
-              <div className="content notification is-light-green">
-                <h2 className="title is-2 has-text-dark">Made right in the SFV</h2>
-                <p>All our free standing candles are made in our home in the beautiful San Fernando Valley</p>
-                <Link to="/candles">
-                  <button className="button is-dark">Browse</button>
-                </Link>
-              </div>
-            </div>
-          </div>
+    
+    <section className="h-screen p-20 bg-purple-400">
+    	<SEO title="Home" />
+    	<div className="grid grid-cols-6 gap-16">
+    		<div className="col-span-3">
+      
+				<img src="https://cdn18.picryl.com/photo/2019/10/07/san-fernando-valley-california-e90e48-1024.jpg"></img>
+			</div>
+			<div className="col-span-3 nm-pink-flat p-5">
+				<h2 className="title is-2 has-text-dark">Made right in the SFV</h2>
+				<p>All our free standing candles are made in our home in the beautiful San Fernando Valley</p>
+			</div>
+			<div className="col-start-3 col-span-2 text-center">
+			<Link to="/candles">
+				<button className="bg-purple-400 w-3/4 text-xl rounded p-2 mt-4">Browse</button>
+			</Link>
+			<div className='col-start-3 col-span-2 text-center text-3xl text-purple-400'>^</div>
+      	</div>
+		</div>
 
-          <div className="columns is-gapless">
-            <div className="column is-6 is-flex">
-              <div className="content notification is-wine">
-                <h2 className="title is-2 has-text-dark">Choose from a combination of colors</h2>
-                <p>Mix colors to create a candle unique to you</p>
-              </div>
-            </div>
-            <div className="column is-6">
-              <figure className="image is-3by2">
-                <img src="https://image.exportimes.com/UploadFile/UploadImg/2017/12/7/201712071031273727.jpg"></img>
-              </figure>
-            </div>
-          </div>
-
-        </div>
-      </div>
+		
+      
     </section>
   </div>
 )
